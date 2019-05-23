b6c3f3b2acd32a58d87002ff0813b6a603559934
Ravikiran303 <ravikiran2221@gmail.com>
2019-05-23 22:17:20 +0530

    Add get API for retreiving books

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/books", (req, res) => {
+  const books = [
+    {
+      image: "https://images-na.ssl-images-amazon.com/images/I/91WJQeRImJL.jpg",
+      title: "Super Heros",
+      author: "Ravi",
+      description: ".........."
+    }
+  ];
+  res.send(books);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
