fdde4805ce57a2627d7480e34acdf8298731ea95
Ravikiran303 <ravikiran2221@gmail.com>
2019-05-23 22:24:47 +0530

    Connect to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+var mongoose = require("mongoose");
+const mongoURI = "mongodb://localhost:27017/Books";
+
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 app.get("/books", (req, res) => {
   const books = [
