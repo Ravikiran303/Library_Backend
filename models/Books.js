a995cb81543297f588470aa4822f7690eacb6bdd
Ravikiran303 <ravikiran2221@gmail.com>
2019-05-24 21:52:10 +0530

    Add Boolean value and retreive if true

--- models/Books.js
+++ models/Books.js
@@ -4,6 +4,10 @@ const Schema = mongoose.Schema;
 const Books_Schema = new Schema({
   title: String,
   author: String,
-  description: String
+  description: String,
+  checkout: {
+    type: Boolean,
+    default: true
+  }
 });
 module.exports = Books = mongoose.model("Books", Books_Schema, "Books");
