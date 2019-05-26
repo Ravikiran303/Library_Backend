30a8940aa0d6c443c82c511d4d731823f3df290d
Ravikiran303 <ravikiran2221@gmail.com>
2019-05-26 13:06:27 +0530

    Add User Schema

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Books_Schema = new Schema({
+  title: String,
+  author: String,
+  description: String,
+  available: {
+    type: Boolean,
+    default: true
+  }
+});
+module.exports = Books = mongoose.model("User", Books_Schema, "Users");
